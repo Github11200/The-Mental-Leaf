018c770fa68a1f15c04ada5d65087a56fa3bab7d
Github11200 <78944617+Github11200@users.noreply.github.com>
2023-08-12 08:08:11 -0700

    Setup the project
    I installed CRA, tailwind css, and daisy UI, and I also made edits to the files from the default CRA template.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App" data-theme="synthwave">
+            <button type="button" className="btn">
+                hello World
+            </button>
+        </div>
+    );
+}
+
+export default App;
