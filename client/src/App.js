01cddb87e340603f356ca4b5aa1f50194476a32a
Github11200 <78944617+Github11200@users.noreply.github.com>
2023-08-14 21:51:27 -0700

    Created a theme changer

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,27 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomepageWrapper } from "./Components/Homepage/homepageWrapper";
 import { DashboardWrapper } from "./Components/Dashboard/dashboardWrapper";
-export let theme = "retro";
+import React, { useState } from "react";
+
+export const themeContext = React.createContext();
 
 function App() {
+    const [theme, setTheme] = useState("retro");
+
     return (
-        <div data-theme={theme}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HomepageWrapper />} />
-                    <Route path="/overview" element={<DashboardWrapper />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <themeContext.Provider value={[theme, setTheme]}>
+            <div data-theme={theme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<HomepageWrapper />} />
+                        <Route
+                            path="/overview"
+                            element={<DashboardWrapper />}
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </themeContext.Provider>
     );
 }
 
--- client/src/Components/Homepage/homepageWrapper.js
+++ client/src/Components/Homepage/homepageWrapper.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { HomepageHeader } from "./homepageHeader";
 import { HomepageMain } from "./homepageMain";
 import logo from "../../Logo.png";
-import theme from "../../App.js";
+import { themeContext } from "../../App.js";
 
 export const HomepageWrapper = () => {
+    const [theme, setTheme] = useContext(themeContext);
+
     return (
         <div className="prose flex min-w-[50vw] h-screen m-auto">
             <div className="self-center" data-theme={theme}>
